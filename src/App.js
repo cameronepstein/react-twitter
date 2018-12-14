b9702e5c7b1319fec071bf3da4f2e8962103af4f
Cameron Epstein <cameron.a.epstein@gmail.com>
2018-12-14 18:17:08 +0000

    request user tweets and log response

--- src/App.js
+++ src/App.js
@@ -14,39 +14,16 @@ class App extends Component {
   }
 
   fetchTweets(user) {
-    let endpoint = "https://www.graphqlhub.com/graphql?query=";
-    let urlParams =`
-    {
-      graphQLHub
-      twitter {
-        user(identifier: name, identity: ${user}) {
-          created_at
-          description
-          id
-          screen_name
-          name
-          profile_image_url
-          url
-          tweets_count
-          followers_count
+    let urlRequest = `https://www.graphqlhub.com/graphql?query=%7B%0A%20%20graphQLHub%0A%20%20twitter%20%7B%0A%20%20%20%20user(identifier%3A%20name%2C%20identity%3A%20%22${user}%22)%20%7B%0A%20%20%20%20%20%20created_at%0A%20%20%20%20%20%20description%0A%20%20%20%20%20%20id%0A%20%20%20%20%20%20screen_name%0A%20%20%20%20%20%20name%0A%20%20%20%20%20%20profile_image_url%0A%20%20%20%20%20%20url%0A%20%20%20%20%20%20tweets_count%0A%20%20%20%20%20%20followers_count%0A%20%20%20%20%20%20%0A%20%20%20%20%20%20tweets(limit%3A%2050)%20%7B%0A%20%20%20%20%20%20%20%20text%0A%20%20%20%20%20%20%09retweet_count%0A%20%20%20%20%20%20%20%20created_at%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%20%20search(q%3A%20%22Javascript%22%2C%20count%3A%201%2C%20result_type%3A%20mixed)%20%7B%0A%20%20%20%20%20%20user%20%7B%0A%20%20%20%20%20%20%20%20screen_name%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20id%0A%20%20%20%20%20%20text%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A`;
 
-          tweets(limit: 50) {
-            text
-          	retweet_count
-            created_at
-          }
-        }
-        search(q: "Javascript", count: 1, result_type: mixed) {
-          user {
-            screen_name
-          }
-          id
-          text
-        }
-      }
-    }`;
-
-  console.log(urlParams);
+    fetch(urlRequest)
+       .then((response) => response.json())
+       .then((responseJson) => {
+        console.log(responseJson);
+       })
+       .catch((error) => {
+         console.error(error);
+       });
   }
 
   render() {
